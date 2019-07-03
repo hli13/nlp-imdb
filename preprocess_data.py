7eaa74072bfe4102afd4fff2dc4515375b1e7d5b
Haoyang (Ryan) Li <l.haoyang.3@gmail.com>
2019-07-02 23:59:33 -0500

    Refactor preprocess_data.py

--- preprocess_data.py
+++ preprocess_data.py
@@ -28,8 +28,9 @@ def read_reviews(data):
     Returns
     -------
     x : list of list
-        a list of all reviews. each item contains one review that is tokenized 
-        and converted into a list of tokens (i.e., words and symbols)
+        a list of tokenized reviews. 
+        Each item contains one review that is tokenized and converted into a 
+        list of tokens (i.e., words and symbols)
         
     """
     # get all of the training/testing reviews (including unlabeled reviews)
@@ -90,64 +91,93 @@ def read_reviews(data):
     return x
 
 
+def build_vocab(x_train, x_test):
+    """
+    Build dictionary/vacabulary based on the training dataset and save to files
+    
+    Parameters
+    ----------
+    x_train : list of list
+        a list of tokenized training reviews. 
+        Each item contains one review that is tokenized and converted into a 
+        list of tokens (i.e., words and symbols)
+    x_test : list of list
+        a list of tokenized testing reviews. 
+        Each item contains one review that is tokenized and converted into a 
+        list of tokens (i.e., words and symbols)
+        
+    Returns
+    -------
+    None
+    """
+    # word_to_id and id_to_word
+    # associate an id to every unique token in the training data
+    all_tokens = itertools.chain.from_iterable(x_train)
+    word_to_id = {token: idx for idx, token in enumerate(set(all_tokens))}
+    
+    all_tokens = itertools.chain.from_iterable(x_train)
+    id_to_word = [token for idx, token in enumerate(set(all_tokens))]
+    id_to_word = np.asarray(id_to_word)
+    
+    ## let's sort the indices by word frequency instead of random
+    x_train_token_ids = [[word_to_id[token] for token in x] for x in x_train]
+    count = np.zeros(id_to_word.shape)
+    for x in x_train_token_ids:
+        for token in x:
+            count[token] += 1
+    indices = np.argsort(-count)
+    id_to_word = id_to_word[indices]
+    count = count[indices]
+    
+    hist = np.histogram(count,bins=[1,10,100,1000,10000])
+    print(hist)
+    for i in range(10):
+        print(id_to_word[i],count[i])
+    
+    print(np.sum(count[0:100]))   
+    print(np.sum(count[0:8000]))
+        
+        
+    ## recreate word_to_id based on sorted list
+    word_to_id = {token: idx for idx, token in enumerate(id_to_word)}
+    
+    ## assign -1 if token doesn't appear in our dictionary
+    ## add +1 to all token ids, we went to reserve id=0 for an unknown token
+    x_train_token_ids = [[word_to_id.get(token,-1)+1 for token in x] for x in x_train]
+    x_test_token_ids = [[word_to_id.get(token,-1)+1 for token in x] for x in x_test]
+    
+    ## save dictionary
+    dict_filename = preproc_dir + '/imdb_dictionary.npy'
+    np.save(dict_filename,np.asarray(id_to_word))
+    
+    ## save training data to single text file
+    train_filename = preproc_dir + '/imdb_train.txt'
+    with io.open(train_filename,'w',encoding='utf-8') as f:
+        for tokens in x_train_token_ids:
+            for token in tokens:
+                f.write("%i " % token)
+            f.write("\n")
+    
+    ## save test data to single text file
+    test_filename = preproc_dir + '/imdb_test.txt'
+    with io.open(test_filename,'w',encoding='utf-8') as f:
+        for tokens in x_test_token_ids:
+            for token in tokens:
+                f.write("%i " % token)
+            f.write("\n")
+    
+
 # read and tokenize training and testing datasets
 print("\nRead and Tokenize IMDb Reviews")
 print("-------------------------------")
 x_train = read_reviews('train')
 x_test = read_reviews('test')
 
+# build dictionary and save to files
+print("\nBuild Dictionary Based on the Training Reviews")
+print("-----------------------------------------------")
+build_vocab(x_train, x_test)
 
-### word_to_id and id_to_word. associate an id to every unique token in the training data
-all_tokens = itertools.chain.from_iterable(x_train)
-word_to_id = {token: idx for idx, token in enumerate(set(all_tokens))}
-
-all_tokens = itertools.chain.from_iterable(x_train)
-id_to_word = [token for idx, token in enumerate(set(all_tokens))]
-id_to_word = np.asarray(id_to_word)
-
-## let's sort the indices by word frequency instead of random
-x_train_token_ids = [[word_to_id[token] for token in x] for x in x_train]
-count = np.zeros(id_to_word.shape)
-for x in x_train_token_ids:
-    for token in x:
-        count[token] += 1
-indices = np.argsort(-count)
-id_to_word = id_to_word[indices]
-count = count[indices]
-
-hist = np.histogram(count,bins=[1,10,100,1000,10000])
-print(hist)
-for i in range(10):
-    print(id_to_word[i],count[i])
-
-print(np.sum(count[0:100]))   
-print(np.sum(count[0:8000]))
-    
-    
-## recreate word_to_id based on sorted list
-word_to_id = {token: idx for idx, token in enumerate(id_to_word)}
-
-## assign -1 if token doesn't appear in our dictionary
-## add +1 to all token ids, we went to reserve id=0 for an unknown token
-x_train_token_ids = [[word_to_id.get(token,-1)+1 for token in x] for x in x_train]
-x_test_token_ids = [[word_to_id.get(token,-1)+1 for token in x] for x in x_test]
-
-## save dictionary
-np.save('preprocessed_data/imdb_dictionary.npy',np.asarray(id_to_word))
-
-## save training data to single text file
-with io.open('preprocessed_data/imdb_train.txt','w',encoding='utf-8') as f:
-    for tokens in x_train_token_ids:
-        for token in tokens:
-            f.write("%i " % token)
-        f.write("\n")
-
-## save test data to single text file
-with io.open('preprocessed_data/imdb_test.txt','w',encoding='utf-8') as f:
-    for tokens in x_test_token_ids:
-        for token in tokens:
-            f.write("%i " % token)
-        f.write("\n")
         
 
 ## GloVe Feature        
